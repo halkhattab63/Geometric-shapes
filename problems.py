fb5c32f5206133f45a5c8033acf153added8c2f6
halkhattab63 <100689695+halkhattab63@users.noreply.github.com>
2024-05-27 17:52:15 +0300

    2.0

--- problems.py
+++ problems.py
@@ -3,8 +3,15 @@
 #  وإذا كان ضلعان فقط متساويان يكون متساوي الساقين وإلا يكون مختلف الأضلاع)
 
 def triangle(x , y , z) : 
-    if x == y and y == z: return f"dfav"
-    elif x == y or y == z or x == z: return f"wevs"
-    else: return f"vsvs"
+    if x == y and y == z: 
+        return f"equilateral triangle"
+    
+    elif x == y or y == z or x == z: 
+        return f"isosception triangle"
+    
+    elif x**2 + y**2  == z**2 or x**2 + z**2 == y**2 or y**2 + z**2 == x**2:
+        return f"right triangle "
+    else: 
+        return f"normal triangle"
 
-print(triangle(4,8,4))
+print(triangle(5,7,3))
